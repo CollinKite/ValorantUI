109431f6a21741e93f8fbae0e913685f6b838f8e
Collin Kite <me@collinkite.com>
2022-11-06 20:05:58 -0700

    Initial Commit

---
+++ src/test/java/kite/collin/valorantui/ValorantUiApplicationTests.java
@@ -0,0 +1,13 @@
+package kite.collin.valorantui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValorantUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
